be96df568e93b2366a5a4bf188df9ec7d651fc51
Abiel MaGu <abielmagu@gmail.com>
2019-06-10 19:28:52 -0500

    Update header.php

--- wb/header.php
+++ wb/header.php
@@ -3,8 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>La Potosina Express | <?= date('Y') ?></title>
-        <meta name="description" content="La Potosina Express es una empresa de servicio de envios con logistica. Nuevo Laredo, Tamaulipas, Mexico">
+        <title>Anisotop | <?= date('Y') ?></title>
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
         <link rel="manifest" href="site.webmanifest">
@@ -12,11 +11,11 @@
         <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/potosinaexpress.css">
+        <link rel="stylesheet" href="assets/css/anisotop.css">
         <link rel="stylesheet" href="assets/fontawesome_5.0.6/css/fontawesome-all.min.css">
     </head>
     <body>
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
-        <h1 style="display: none">La Potosina Express</h1>
+        <h1 style="display: none">Anisotop</h1>
